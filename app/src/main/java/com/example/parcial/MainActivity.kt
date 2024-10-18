97b77cc40843c1de6e779b7254beb550f06b10cb
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-10-18 00:16:51 -0500

    implementando el activity_main.xml y el main para empezar

--- app/src/main/java/com/example/parcial/MainActivity.kt
+++ app/src/main/java/com/example/parcial/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.parcial
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Cargar el fragmento de bienvenida al inicio
+        if (savedInstanceState == null) {
+            loadFragment(WelcomeFragment())
         }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
 }
+
